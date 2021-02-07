125e5312db8ef27c80accd320c8bec640f26e9ef
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-02-06 20:05:48 -0500

    use reducer hook

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,8 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
+import { ADD_TO_FAVORITES } from '../global/Types';
+
+const initialState = {
+    favorites: [],
+};
+
+const favoriteReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case ADD_TO_FAVORITES:
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload ]
+            }
+            break;
+    
+        default:
+            return state;
+    }
+}
 
 const Characters = () => {
 
     const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+    const handleClick = (favorite: any) => {
+        dispatch({type: ADD_TO_FAVORITES, payload: favorite});
+    }
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
@@ -13,8 +37,13 @@ const Characters = () => {
 
     return (
         <div>
+            {favorites.favorites.map((favorite) => ())}
+
            {characters.map((character: any) => (
-               <h2>{character?.name}</h2>
+               <div key={character.id}>
+                   <h2>{character?.name}</h2>
+                   <button type="button" onClick={() => handleClick(character)}>Agregar a favorito</button>
+               </div>
            ))}
         </div>
     )
